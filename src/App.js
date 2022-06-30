ed117cec6b6a9b789ac5e282ed53b884639fe575
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-06-30 12:23:41 +0800

    initial webcam config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import React, {useRef} from 'react';
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const cameraRef = useRef(null);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zindex:9,
+          width:640,
+          height:480
+        }}
+        />
+        <canvas ref={cameraRef}
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zindex:9,
+          width:640,
+          height:480
+        }}/>
       </header>
     </div>
   );
